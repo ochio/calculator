ae5454edfaec89339efefb6372e5aac7f0994a8f
ochi <ocos54ce@gmail.com>
2024-06-04 22:09:23 +0900

    wrap by FocusContext.Provider

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,11 +1,21 @@
+import { FocusContext, useFocusable } from "@noriginmedia/norigin-spatial-navigation"
 import { NumberButton } from "./NumberButton"
+import { useEffect } from "react";
 
 export const Board = () => {
+  const { ref, focusKey, focusSelf } = useFocusable();
+  useEffect(() => {
+    focusSelf();
+  }, [focusSelf]);
+
+
   return (
-    <div className="gap-2 grid grid-cols-3 grid-rows-3 grid-flow-row">
-      {Array(10).fill(null).map((_,i) => {
-        return <NumberButton key={i} num={9-i} defaultClass={"w-15 bg-white text-black"} focusedClass={'bg-green-200'}/>
-      })}
-    </div>
+    <FocusContext.Provider value={focusKey}>
+      <div className="gap-2 grid grid-cols-3 grid-rows-3 grid-flow-row" ref={ref}>
+        {Array(10).fill(null).map((_,i) => {
+          return <NumberButton key={i} num={9-i} defaultClass={"w-15 bg-white text-black"} focusedClass={'bg-green-200'}/>
+        })}
+      </div>
+    </FocusContext.Provider>
   )
 }
